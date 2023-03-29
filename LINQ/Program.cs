e9771fe6bd3d9e34bbd382835400cd0f92048868
daisy.omoikirodah <thesmexxiichic@gmail.com>
2023-03-29 11:19:38 -0400

    done

--- LINQ/Program.cs
+++ LINQ/Program.cs
@@ -4,7 +4,24 @@
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello, World!");
+            var videoGamesList = new List<string> { "callofduty", "tekken", "godofwar", "grandtheftauto" };
+
+            Console.WriteLine("Original List");
+
+            videoGamesList.ForEach(Console.WriteLine);
+            Console.WriteLine();
+            Console.WriteLine("Ordered List:");
+            videoGamesList.OrderBy(name => name.Length).ToList().ForEach(gameName => Console.WriteLine(gameName));
         }
-    }
-}
+
+             
+    }       
+    }   
+
+
+
+
+
+
+          
+
